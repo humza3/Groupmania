31851771abf016031e5c23171d3265e816c02553
humza3 <humzabutt3@gmail.com>
2020-09-25 13:31:37 +0100

    added mariadb connector

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mariadb = require('mariadb');
 
 const userRoutes = require('./routes/user');
 
 const app = express();
+
+const pool = mariadb.createPool({
+     host: '127.0.0.1', 
+     user:'root', 
+     password: 'password',
+     connectionLimit: 5
+});
+async function asyncFunction() {
+  let conn;
+  try {
+	conn = await pool.getConnection();
+	const rows = await conn.query("SELECT 1 as val");
+	console.log('Successfully connected to MariaDB!');
+	console.log(rows); //[ {val: 1}, meta: ... ]
+	const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+	console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+  } catch (err) {
+	console.log('Unable to connect to MariaDB!');
+	throw err;
+  } finally {
+	if (conn) return conn.end();
+  }
+}
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
