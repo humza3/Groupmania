527864c9ada165f9e4ff102cbb11a7b05a5fbfda
humza3 <humzabutt3@gmail.com>
2020-10-09 00:40:23 +0100

    changed registartion page to sign up page

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/Signup', userCtrl.signup);
+router.post('/Login', userCtrl.login);
 
 module.exports = router;
