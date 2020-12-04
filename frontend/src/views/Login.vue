b03b5a3e814c0dbb3c2dd54bed7b1fdc158a6b02
humza3 <humzabutt3@gmail.com>
2020-12-04 13:16:26 +0000

    updataed login page to work, to submit details to mariadb

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,17 +3,54 @@
     <h1>This is a login page</h1>
 	<form>
 		<div class="container">
-			<label for="uname"><b>Email Address: </b></label>
-			<input type="text" placeholder="Enter Email Address" name="uemail" required>
+			<label for="email"><b>Email Address: </b></label>
+			<input type="email" id="email" v-model="email" placeholder="Enter Email Address" name="email" required>
 			<br>
-			<label for="psw"><b>Password: </b></label>
-			<input type="password" placeholder="Enter Password" name="psw" required>
+			<label for="password"><b>Password: </b></label>
+			<input type="password" id="password" v-model="password" placeholder="Enter Password" name="password" required>
 			<br>
-			<button type="submit" id="submit">Login</button>    
+			<button type="submit" v-on:click="login" id="submit">Sign Up</button>    
 		</div>
 	</form>
   </div>
 </template>
+
+<script>
+const axios = require('axios');
+
+export default {
+    name: 'login',
+    data() {
+        return {
+            email: "",
+            password: ""
+        }   
+    },
+    methods: {
+        login: function(e) {
+			e.preventDefault();
+			this.email = document.getElementById('email').value;
+			this.password = document.getElementById('password').value;
+            if (this.email != "" && this.password !="") {
+                axios.post('http://localhost:3000/api/auth/login', {
+					email: this.email,
+					password: this.password
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                 })
+            }
+            else {
+                console.log('You must enter your correct username and / or password!')
+            }
+        }
+           
+    }
+}
+</script>
 
 <style lang="scss">
 form {
